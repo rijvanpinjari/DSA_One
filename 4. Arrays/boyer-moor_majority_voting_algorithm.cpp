022b6c0d01f116e74ad3a349861309a6c6937bef
Rijvan Pinjari <rijvanpinjari0786@gmail.com>
2023-11-04 23:16:10 +0530

    Problem Solved

---
+++ 4. Arrays/boyer-moor_majority_voting_algorithm.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find majority element
+int find_Majority(int arr[], int n)
+{
+    int i, element = -1, count = 0;
+
+    // Finding majority candidate
+    for (i = 0; i < n; i++)
+    {
+        if (count == 0)
+        {
+            element = arr[i];
+            count = 1;
+        }
+        else
+        {
+            if (arr[i] == element)
+                count++;
+            else
+                count--;
+        }
+    }
+    int count1 = 0;
+
+    // Checking if majority candidate occurs more than n/2 times
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == element)
+            count1++;
+    }
+
+    if (count1 > n / 2)
+        return element;
+    return -1;
+}
+int main()
+{
+    int arr[] = {1, 1, 1, 1, 2, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int majority = find_Majority(arr, n);
+    cout << " The majority element is : " << majority;
+    return 0;
+}
